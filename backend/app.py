99bf67198e31366bf69086dc12abaf148fc75804
RayWolf-0 <topalterluz.00.b@gmail.com>
2026-07-29 16:43:31 -0400

    se escribe codigo de config, app, models y se comienza a escribir image_Service

---
+++ backend/app.py
@@ -0,0 +1,44 @@
+import os
+import pytesseract
+from flask import Flask
+from config import Config
+from models import db, RolUsuario, EstadoDocumento
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+#Tesseract
+pytesseract.pytesseract.tesseract_cmd = app.config['TESSERACT_CDM']
+
+#ORM
+db.init_app(app)
+
+#Apartado de creacion de carpetas (si no están)
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True),
+os.makedirs(app.config['PDFS_FOLDER'], exist_ok=True),
+os.makedirs(app.config['FIRMAS_FOLDER'], exist_ok=True),
+os.makedirs(os.path.join(app.root_path, 'instance'), exist_ok=True)
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+    if not RolUsuario.query.first():
+        db.session.add_all([
+            RolUsuario(nombre_rol = 'VENDEDOR'),
+            RolUsuario(nombre_rol = 'SUPERVISOR') 
+        ])
+    
+    if not EstadoDocumento.query.first():
+        db.session.add_all([ 
+            EstadoDocumento(nombre_estado = 'PENDIENTE'),
+            EstadoDocumento(nombre_estado = 'EN_REVISION'),
+            EstadoDocumento(nombre_estado = 'APROVADO'),
+            EstadoDocumento(nombre_estado = 'RECHAZADO')                     
+        ])
+        
+    db.session.commit()
+    print("Base de datos SQLite y Carpetas 'storage/' creadas")
+        
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
---
+++ backend/config.py
@@ -0,0 +1,19 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = 'clave_secreta_para_desarrollo'
+    
+    #base de datos sqlite
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'scanner.db').replace('\\','/')
+    SQLALCHEMY_TRAK_MODIFICATIONS = False
+    
+    #Tesseract
+    TESSERACT_CDM = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    
+    #Carpetas de Almacenamiento
+    STORAGE_FOLDER = os.path.join(BASE_DIR, 'storage')
+    UPLOAD_FOLDER = os.path.join(STORAGE_FOLDER, 'uploads')
+    PDFS_FOLDER = os.path.join(STORAGE_FOLDER, 'pdf_generado')
+    FIRMAS_FOLDER = os.path.join(STORAGE_FOLDER, 'firmas')
